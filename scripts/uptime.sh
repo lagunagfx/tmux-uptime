cb114dcef59bcb6f09c33fff9409b131a5123cb3
Jorge Barrientos Poisson <lagunagfx@lagunagfx.com>
2026-05-26 17:42:21 +0200

    Use general uptime syntax without the --pretty option

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -1,34 +1,63 @@
 #!/usr/bin/env bash
 
 # Description : an abbreviated form for uptime, suitable for tmux status bar
-#
 # written by Jorge Barrientos AKA LagunaGFX
+# 
+# ChangeLog :
+# 2026.05.26 - Use general 'uptime' outpur as source since macOS lacks the '--pretty' option.
 
-extract() {
-	local word="$1"
+if [ -z $( which uptime ) ];
+then
 
-	if [ ! -z "$word" ];
+	# the 'uptime' binary is not present in the system
+	printf "-not available-"
+	# Exceptional Case EXIT
+else
+	# General Case START
+	UPTIME=$( uptime | awk -F "up " '{print $2}' | awk -F " user" '{print $1}' )
+
+	# Example cases extracted from macOS terminal 10.15.8 ... Are they different from Linux' GNU uptime
+	#
+	# EXAMPLE="15:31  up 37 secs, 1 user, load averages: 2.56 0.69 0.26"
+	# EXAMPLE="16:04  up 34 mins, 1 user, load averages: 1.72 1.73 1.36"
+	# EXAMPLE="16:31  up 1 hr, 1 user, load averages: 3.05 2.06 1.92"
+	# EXAMPLE="17:17  up  1:47, 1 user, load averages: 0.34 0.36 0.41"
+	#
+	# UPTIME=$( printf "%s" "$EXAMPLE" | awk -F "up " '{print $2}' | awk -F " user" '{print $1}' )
+
+	if [ ! -z $( printf "%s" $UPTIME | grep day ) ];
 	then
-		local uptime="$( uptime --pretty | sed 's/up //' | grep "$word" | awk -F " $word" '{print $1}' | rev | cut -d " " -f1 | rev )"
-		if [ ! -z "$uptime" ];
+		# time elapsed : MORE than ONE DAY
+		printf "%sd " $( printf "%s" $UPTIME | awk -F "day" '{print $1}' )
+		HOUR_AND_MINUTE=$( printf "%s" $UPTIME | awk -F "day" '{print $2}' | cut -d ',' -f 2 )
+	else
+		HOUR_AND_MINUTE=$( printf "%s" $UPTIME | cut -d ',' -f 1 )
+	fi
+
+	if [ ! -z $( printf "%s" $UPTIME | grep hr ) ];
+	then
+		# time elapsed is EXACTLY 1 hour
+		printf "1h 0m"
+	else
+		# time elapsed : LESS than ONE HOUR
+		if [ ! -z $( printf "%s" $UPTIME | grep min ) ];
 		then
-			local suffix=
-			printf "%s%s" $uptime $( echo "$word" | cut -b1 )
-	 		if [ "$word" != "minute" ];
+			# time elapsed : MORE OR EQUAL than ONE MINUTE
+			printf "%sm" $( printf "%s" $UPTIME | awk -F "min" '{print $1}' )
+		else
+			if [ ! -z $( printf "%s" $UPTIME | grep sec ) ];
 			then
-				printf " "
+				# time elapsed : LESS than ONE MINUTE
+				printf "%ss" $( printf "%s" "$UPTIME" | awk -F " sec" '{print $1}' )
+			else
+				# general case : any HOURS and MINUTES elapsed
+				HOUR=$( printf "%s" $HOUR_AND_MINUTE | cut -d ':' -f 1 )
+				MINUTE=$( printf "%s" $HOUR_AND_MINUTE | cut -d ':' -f 2 )
+				printf "%sh %sm" $HOUR $MINUTE
 			fi
 		fi
 	fi
-}
+	# General Case END
+fi
 
-main() {
-	extract year
-	extract month
-	extract week
-	extract day
-	extract hour
-	extract minute
-}
-
-main
+# End of Script
